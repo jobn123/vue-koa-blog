6577594700528e222c3be09a220ae9dde7049d2e
zhoubin <jobn_joy@163.com>
2016-11-20 15:38:17 +0800

    adjust floder structure

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,7 +12,7 @@ var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './app/client/main.js'
   },
   output: {
     path: config.build.assetsRoot,
