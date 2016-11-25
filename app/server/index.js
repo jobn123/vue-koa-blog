94b113ee68704af70a527ae652910482a5bcf484
zhoubin <jobn_joy@163.com>
2016-11-25 21:46:38 +0800

    koa router

--- app/server/index.js
+++ app/server/index.js
@@ -4,25 +4,26 @@ const koa = require('koa'),
       bodyParser = require('koa-bodyparser'),
       router = require('koa-router')(),
       co = require('co'),
-      mongoose = require('mongoose'),
-      User = mongoose.model("User");
+      controllers = require('./controllers/index.js'),
+      mongoose = require('mongoose');
+      // User = mongoose.model("User");
 // app.use(function *(){
 //   this.body = 'hello world'
 // })
-router.get('/', function* (next){
-  var User = new User({
-    uid
-  })
-  this.body = yield User.find({});
-})
-// co(function* (){
-  // app.use(bodyParser())
-  // yield controllers.init(router)
+// router.get('/', function* (next){
+//   var User = new User({
+//     uid
+//   })
+//   this.body = yield User.find({});
+// })
+co(function* (){
+  app.use(bodyParser())
+  yield controllers.init(router)
   console.log('hihihihihi3');
 
-  // app.use(router.routes())
+  app.use(router.routes())
 
-  app.listen(4000, ()=>{
+  app.listen(5000, ()=>{
     console.log('port start at 4000');
   })
-// })
+})
